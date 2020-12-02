7bc722583fe7a88d18b5d29a4b454afcf909ba8a
BrandonWerbel <brandon@werbel.org>
2020-12-02 18:12:10 -0500

    Update automation script

--- _subscription_process/automation.py
+++ _subscription_process/automation.py
@@ -1,5 +1,19 @@
 from SendEmail import SendEmail
 
 em = SendEmail()
-message = em.create_message('me', 'brandon@werbel.org', 'I hope this works', 'Hello, Brandon. I hope you get this!')
-em.send_message('me', message)
+message_body = """Hello there!
+
+You are getting this email because you are subscribed to Brandon Werbel's Israel Blog. There has just been a new post, check it out!
+
+If you would like to unsubscribe from this site, respond to this email saying so. There is not currently an automated way to do so, but it should be coming soon!"""
+
+
+with open('subscriber_list.txt', 'r') as subscribers:
+    subsArray = subscribers.readlines()
+    for line in subsArray[:len(subsArray) - 1]:
+        line = line[0: len(line) - 1]
+        message = em.create_message('me', line, 'New Post in Brandon\'s Blog!', message_body)
+        em.send_message('me', message)
+    line = subsArray[len(subsArray) - 1]
+    message = em.create_message('me', line, 'New Post in Brandon\'s Blog!', message_body)
+    em.send_message('me', message)
