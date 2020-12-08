6299c52dbd94f71bf6fcc57b3e4c621f0ad21cda
BrandonWerbel <brandon@werbel.org>
2020-12-08 13:07:09 -0500

    Fix paths in decryption script

--- _subscription_process/decrypt-credentials.sh
+++ _subscription_process/decrypt-credentials.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CREDENTIALS_PASSPHRASE" \
---output ../../_subscription_process/credentials.json ../../_subscription_process/encrypted-credentials.json.gpg
+--output _subscription_process/credentials.json _subscription_process/encrypted-credentials.json.gpg
