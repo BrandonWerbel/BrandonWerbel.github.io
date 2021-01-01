c0eeba413eda3a0f3251d0c51cfa22ceb49bbc1d
BrandonWerbel <brandon@werbel.org>
2021-01-01 15:56:40 -0500

    Implement Subscribing Process in Site

---
+++ _subscription_process/GapiSetup.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+from pybase64 import urlsafe_b64encode
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/spreadsheets.readonly']
+spreadsheet_id = '1pPd2Xws3oO0v3EfAB7ToX9x7yxw6nUHUUQW5zRkIKVo'
+
+class GapiSetup:
+
+    def __init__(self):
+        """Shows basic usage of the Gmail API.
+        Lists the user's Gmail labels.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.gmailService = build('gmail', 'v1', credentials=creds)
+        self.sheetsService = build('sheets', 'v4', credentials=creds)
+
+    def create_message(self, sender, to, subject, message_text):
+        """Create a message for an email.
+
+        Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+        Returns:
+        An object containing a base64url encoded email object.
+        """
+        message = MIMEText(message_text)
+        message['to'] = to
+        message['from'] = sender
+        message['subject'] = subject
+        raw = urlsafe_b64encode(message.as_bytes())
+        raw = raw.decode()
+        return {'raw': raw}
+
+    def send_message(self, user_id, message):
+        """Send an email message.
+
+        Args:
+            user_id: User's email address. The special value "me"
+            can be used to indicate the authenticated user.
+            message: Message to be sent.
+
+        Returns:
+            Sent Message.
+        """
+        try:
+            message = (self.gmailService.users().messages()
+                .send(userId=user_id, body=message)
+                .execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
+    def get_emails(self):
+        return self.sheetsService.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='B2:B1000', majorDimension='COLUMNS').execute()['values'][0]
--- _subscription_process/automation.py
+++ _subscription_process/automation.py
@@ -1,19 +1,14 @@
-from SendEmail import SendEmail
+from GapiSetup import GapiSetup
 
-em = SendEmail()
+gs = GapiSetup()
 message_body = """Hello there!
 
 You are getting this email because you are subscribed to Brandon Werbel's Israel Blog. There has just been a new post, check it out!
 
 If you would like to unsubscribe from this site, respond to this email saying so. There is not currently an automated way to do so, but it should be coming soon!"""
 
+emails = gs.get_emails()
 
-with open('../subscriber_list.txt', 'r') as subscribers:
-    subsArray = subscribers.readlines()
-    for line in subsArray[:len(subsArray) - 1]:
-        line = line[0: len(line) - 1]
-        message = em.create_message('me', line, 'New Post in Brandon\'s Blog!', message_body)
-        em.send_message('me', message)
-    line = subsArray[len(subsArray) - 1]
-    message = em.create_message('me', line, 'New Post in Brandon\'s Blog!', message_body)
-    em.send_message('me', message)
+for email in emails:
+    message = gs.create_message('me', email, 'New Post in Brandon\'s Blog!', message_body)
+    gs.send_message('me', message)
