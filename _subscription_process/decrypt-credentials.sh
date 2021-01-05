fc51e8b42ac882e8315f3b641b70bba3e8470376
BrandonWerbel <brandon@werbel.org>
2021-01-05 14:35:32 -0500

    Encrypt token.pickle file

--- _subscription_process/decrypt-credentials.sh
+++ _subscription_process/decrypt-credentials.sh
@@ -4,3 +4,6 @@
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CREDENTIALS_PASSPHRASE" \
 --output _subscription_process/credentials.json _subscription_process/encrypted-credentials.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$CREDENTIALS_PASSPHRASE" \
+--output _subscription_process/token.pickle _subscription_process/encrypted-token.pickle.gpg
