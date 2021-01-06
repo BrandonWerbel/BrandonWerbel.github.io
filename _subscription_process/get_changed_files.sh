3d7a56a2a3bde406773bfbbc13dc8dc2d0a33b05
BrandonWerbel <brandon@werbel.org>
2021-01-06 11:37:00 -0500

    Remove sh curly brackets

--- _subscription_process/get_changed_files.sh
+++ _subscription_process/get_changed_files.sh
@@ -1,5 +1,5 @@
 touch file_names.txt
 # files_changed=$(/usr/bin/git diff-tree --no-commit-id --name-only -r $(/usr/bin/git log --format="%H" -n 1))
-files_changed=${{ steps.Jitterbug.outputs.all }}
+files_changed=$steps.Jitterbug.outputs.all
 echo "$files_changed" > file_names.txt
 echo "::debug:: $files_changed"
