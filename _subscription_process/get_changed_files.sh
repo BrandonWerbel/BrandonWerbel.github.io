fcc491c1578c6cbe8b0a91c619e972021f3157d1
BrandonWerbel <brandon@werbel.org>
2021-01-06 11:18:16 -0500

    Use jitterbit action to get changed files

--- _subscription_process/get_changed_files.sh
+++ _subscription_process/get_changed_files.sh
@@ -1,4 +1,5 @@
 touch file_names.txt
-files_changed=$(/usr/bin/git diff-tree --no-commit-id --name-only -r $(/usr/bin/git log --format="%H" -n 1))
+# files_changed=$(/usr/bin/git diff-tree --no-commit-id --name-only -r $(/usr/bin/git log --format="%H" -n 1))
+files_changed=${{ steps.abc.outputs.all }}
 echo "$files_changed" > file_names.txt
 echo "::debug:: $files_changed"
