3d79d32fa675d2e09733853d58ea443b9bc932c1
BrandonWerbel <brandon@werbel.org>
2021-01-06 10:45:24 -0500

    Test commit

--- _subscription_process/get_changed_files.sh
+++ _subscription_process/get_changed_files.sh
@@ -1,3 +1,4 @@
 touch file_names.txt
 files_changed=$(/usr/bin/git diff-tree --no-commit-id --name-only -r $(/usr/bin/git log --format="%H" -n 1))
 echo "$files_changed" > file_names.txt
+echo "::debug:: $files_changed"
