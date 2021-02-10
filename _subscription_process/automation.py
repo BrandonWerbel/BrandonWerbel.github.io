67219d59fb9234614aa8e7d6ab21b693a32c590a
BrandonWerbel <brandon@werbel.org>
2021-02-10 09:34:36 -0500

    Only send emails if file has been added

--- _subscription_process/automation.py
+++ _subscription_process/automation.py
@@ -17,23 +17,24 @@ for name in names:
     if name[:12] == "_blog_posts/":
         post_names.append(name[12:22])
 
-highestTimestamp = 0.0
-highestDate = ""
-for name in post_names:
-    date = datetime.strptime(name, '%Y-%m-%d')
-    timestamp = datetime.timestamp(date)
-    if timestamp > highestTimestamp:
-        highestTimestamp = timestamp
-        highestDate = name
+if len(post_names > 0):
+    highestTimestamp = 0.0
+    highestDate = ""
+    for name in post_names:
+        date = datetime.strptime(name, '%Y-%m-%d')
+        timestamp = datetime.timestamp(date)
+        if timestamp > highestTimestamp:
+            highestTimestamp = timestamp
+            highestDate = name
 
-message_body_plain = open("message_body_plain.txt", "r").read().format(highestDate)
+    message_body_plain = open("message_body_plain.txt", "r").read().format(highestDate)
 
-message_body_html = open("message_body_html.txt", "r").read().format(highestDate)
+    message_body_html = open("message_body_html.txt", "r").read().format(highestDate)
 
-emails = gs.get_emails()
+    emails = gs.get_emails()
 
-message = gs.create_message_html('me', 'brandon@werbel.org', 'New Post in Brandon\'s Blog!', message_body_plain, message_body_html)
-gs.send_message('me', message)
-# for email in emails:
-#     message = gs.create_message_html('me', email, 'New Post in Brandon\'s Blog!', message_body_plain, message_body_html)
-#     gs.send_message('me', message)
+    message = gs.create_message_html('me', 'brandon@werbel.org', 'New Post in Brandon\'s Blog!', message_body_plain, message_body_html)
+    gs.send_message('me', message)
+    # for email in emails:
+    #     message = gs.create_message_html('me', email, 'New Post in Brandon\'s Blog!', message_body_plain, message_body_html)
+    #     gs.send_message('me', message)
